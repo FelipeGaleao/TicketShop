0718633fbdb5198f8ede57c86d7c56d124bd9f3d
maycon felipe mota <mayconfelipemotamw3@gmail.com>
2022-08-17 22:25:23 -0400

    feat: adicionado lógica para calcular o valor do ingresso

--- app/src/main/java/com/meicansoft/ticketshop/MainActivity.java
+++ app/src/main/java/com/meicansoft/ticketshop/MainActivity.java
@@ -1,10 +1,13 @@
 package com.meicansoft.ticketshop;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnCalcular;
     private RadioGroup radioGroup;
     private Double extra = 0.0;
+    private int checkedId;
 
     @Override
 
@@ -34,5 +38,50 @@ public class MainActivity extends AppCompatActivity {
         txtResultado = findViewById(R.id.txt_resultado);
         btnCalcular = findViewById(R.id.btn_calcular);
         radioGroup = findViewById(R.id.radio_group);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                    if(checkedId == R.id.radioVip){
+                        extra = 60.0;
+                    }else{
+                        extra = 0.0;
+                    }
+            }
+        });
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int item = spinner.getSelectedItemPosition();
+               try{
+                   int qtdMeio = Integer.parseInt(edtMeio.getText().toString());
+                   int qtdInteiro = Integer.parseInt(edtInteiro.getText().toString());
+                   double valor = extra;
+
+                   switch(item){
+                       case 0:
+                           valor = valor+40;
+                           valor = (valor+qtdMeio/2)+(valor+qtdInteiro);
+                       case 1:
+                           valor = valor+50;
+                           valor = (valor+qtdMeio/2)+(valor+qtdInteiro);
+                       case 2:
+                           valor = valor+60;
+                           valor = (valor+qtdMeio/2)+(valor+qtdInteiro);
+                       case 3:
+                           valor = valor+70;
+                           valor = (valor+qtdMeio/2)+(valor+qtdInteiro);
+                   }
+
+                txtResultado.setText(Double.toString(valor));
+
+               }catch(Exception e){
+
+               }
+
+            }
+        });
+
     }
 }
